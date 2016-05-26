3efc09b48bb1b7f8f52deb591d115db81f75fc65
Ion <ureche.ion@gmail.com>
2016-05-26 13:26:25 +0300

    Added badge system, badge icons.

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -7,6 +7,8 @@
 
   HeaderController.$inject = ['$scope', '$state', 'Authentication', 'menuService'];
 
+  var RatingTresholds = {R1 : 0, R2 : 100, R3 : 200};
+
   function HeaderController($scope, $state, Authentication, menuService) {
     var vm = this;
 
@@ -15,6 +17,8 @@
     vm.isCollapsed = false;
     vm.menu = menuService.getMenu('topbar');
 
+    vm.RatingStyle = GetRatingStyle(Authentication.user.score);
+    vm.badgeImageURL = GetBadgeByRating(Authentication.user.score);
     $scope.$on('$stateChangeSuccess', stateChangeSuccess);
 
     function stateChangeSuccess() {
@@ -22,4 +26,29 @@
       vm.isCollapsed = false;
     }
   }
+
+  function GetBadgeByRating(score)
+  {
+    var rating = parseInt(score);
+    var iconUrl = "modules/users/client/img/badges/badge";
+    var imageIndex="1";
+
+    if(rating >= RatingTresholds.R3) imageIndex = "3"; else
+    if(rating >= RatingTresholds.R2) imageIndex = "2";
+
+    return iconUrl + imageIndex + ".png";
+  }
+
+  function GetRatingStyle(score)
+  {
+    var rating = parseInt(score);
+    var ccolor = 'black';
+
+    if(rating >= RatingTresholds.R3) ccolor = 'red'; else
+    if(rating >= RatingTresholds.R2) ccolor = 'deepskyblue'; else
+    if(rating >= RatingTresholds.R1)  ccolor = 'grey';
+
+    return {'color' : ccolor};
+  }
+
 }());
